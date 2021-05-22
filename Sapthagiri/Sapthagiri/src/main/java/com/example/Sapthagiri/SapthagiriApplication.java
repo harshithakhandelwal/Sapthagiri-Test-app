e3855a9c69de724ea9d7d6dbb8ca8e6334b76a65
Harshitha Khandelwal <Harshitha.Khandelwal@bottomline.com>
2021-05-22 12:20:11 +0530

    Initial commit

---
+++ Sapthagiri/Sapthagiri/src/main/java/com/example/Sapthagiri/SapthagiriApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sapthagiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapthagiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapthagiriApplication.class, args);
+	}
+
+}
---
+++ Sapthagiri/Sapthagiri/src/test/java/com/example/Sapthagiri/SapthagiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sapthagiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapthagiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
